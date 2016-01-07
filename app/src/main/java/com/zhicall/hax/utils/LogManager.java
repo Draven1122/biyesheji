349342a06a2512c91938af093265b5333a0be045
Draven1122 <hjx931122@vip.qq.com>
2016-01-07 16:46:53 +0800

    update

--- app/src/main/java/com/zhicall/hax/utils/LogManager.java
+++ app/src/main/java/com/zhicall/hax/utils/LogManager.java
@@ -5,6 +5,7 @@ import android.util.Log;
 /**
  * Created by Xingchen on 2015/12/21.
  * Email:huangjinxin@zhicall.cn
+ * phone:157571770**
  */
 public class LogManager {
   public static void showLog(String str) {
