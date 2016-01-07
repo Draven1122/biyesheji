a729c3a3b5f29a28a801be0638716076d4faae87
Draven1122 <hjx931122@vip.qq.com>
2016-01-07 17:07:34 +0800

    update

--- app/src/main/java/com/zhicall/hax/BaseActivity.java
+++ app/src/main/java/com/zhicall/hax/BaseActivity.java
@@ -1,14 +1,37 @@
 package com.zhicall.hax;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 /**
  * Created by Xingchen on 2016/1/7.
  * Email:huangjinxin@zhicall.cn
  */
-public class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity {
+  ProgressDialog mProgressDialog = null;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    init();
+  }
+
+  /**
+   * 做一些初始化数据的操作
+   */
+  public abstract void init();
+
+  public void showProgressdialog(String message) {
+    if (mProgressDialog == null) mProgressDialog = new ProgressDialog(BaseActivity.this);
+    mProgressDialog.setMessage(message);
+    mProgressDialog.show();
+  }
+
+  public void showProgressdialog() {
+    showProgressdialog("");
+  }
+
+  public void dissmissProgressDialog() {
+    if (mProgressDialog != null) mProgressDialog.dismiss();
   }
 }
