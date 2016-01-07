f6f4ebb3507340006e8ac21c8fd1f4da2eba8ec6
Draven1122 <hjx931122@vip.qq.com>
2016-01-07 17:01:15 +0800

    add

---
+++ app/src/main/java/com/zhicall/hax/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.zhicall.hax;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Xingchen on 2016/1/7.
+ * Email:huangjinxin@zhicall.cn
+ */
+public class BaseActivity extends Activity {
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
