4f80be6cceb7045103ebc0237f4ed7805c4c1b24
Draven1122 <hjx931122@vip.qq.com>
2016-01-08 10:17:49 +0800

    add
    add the LoginActivity

---
+++ app/src/main/java/com/zhicall/hax/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.zhicall.hax.activities;
+
+import android.os.Bundle;
+import com.zhicall.hax.BaseActivity;
+import com.zhicall.hax.R;
+
+/**
+ * Created by Xingchen on 2016/1/8.
+ * Email:huangjinxin@zhicall.cn
+ */
+public class LoginActivity extends BaseActivity {
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    initActionbar(true,false,"登录");
+  }
+}
--- app/src/main/java/com/zhicall/hax/activities/MainActivity.java
+++ app/src/main/java/com/zhicall/hax/activities/MainActivity.java
@@ -1,14 +1,17 @@
 package com.zhicall.hax.activities;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import butterknife.Bind;
 import com.zhicall.hax.BaseActivity;
 import com.zhicall.hax.R;
 
-/**
+/**APP首页
  * Created by Xingchen on 2016/1/7.
  * Email:huangjinxin@zhicall.cn
  */
 public class MainActivity extends BaseActivity {
+  @Bind(R.id.rlayout_fragmengt_container) RelativeLayout mFragmentContainer;
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
