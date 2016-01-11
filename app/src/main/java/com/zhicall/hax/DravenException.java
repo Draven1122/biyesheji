d426f5a296b7ba44a4ab0718ffd7f27fa260ef7f
Draven1122 <hjx931122@vip.qq.com>
2016-01-11 17:00:02 +0800

    update

---
+++ app/src/main/java/com/zhicall/hax/DravenException.java
@@ -0,0 +1,11 @@
+package com.zhicall.hax;
+
+/**
+ * Created by Xingchen on 2016/1/11.
+ * Email:huangjinxin@zhicall.cn
+ */
+public class DravenException extends RuntimeException {
+  DravenException(String msg) {
+    super(msg);
+  }
+}
