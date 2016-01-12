889b23770a9ef7434902080a4f4f9158aa82168f
Draven1122 <hjx931122@vip.qq.com>
2016-01-12 10:15:21 +0800

    update

--- app/src/main/java/com/zhicall/hax/fragments/InfoFragment.java
+++ app/src/main/java/com/zhicall/hax/fragments/InfoFragment.java
@@ -1,47 +1,76 @@
 package com.zhicall.hax.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.zhicall.hax.R;
-import com.zhicall.hax.net.Data;
-import com.zhicall.hax.net.INewsService;
-import com.zhicall.hax.utils.ToastManager;
+import com.zhicall.hax.bean.NewsSummary;
+import com.zhicall.hax.common.CommonAdapter;
+import com.zhicall.hax.common.CommonViewHolder;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Xingchen on 2016/1/11.
  * Email:huangjinxin@zhicall.cn
  */
 public class InfoFragment extends Fragment {
-  @Bind(R.id.btn_test) Button mTestButton;
+
   private View view;
+  @Bind(R.id.lstv_news) PullToRefreshListView mPullToRefreshListView;
+  @Bind(R.id.v_line1) View mLineView1;
+  @Bind(R.id.v_line2) View mLineView2;
+  @Bind(R.id.v_line3) View mLineView3;
+  @Bind(R.id.v_line4) View mLineView4;
+  private List<View> lineList = new ArrayList<View>();
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
 
   public InfoFragment() {
     super();
-  }
-
-  @OnClick(R.id.btn_test) public void onTestButtonClicked() {
-    Data.service(INewsService.class)
-        .getNewsCategory()
-        .subscribe(reselut -> ToastManager.showToast(reselut.getStatus() + ""));
   }
 
   @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     if (view == null) view = inflater.inflate(R.layout.fragment_info, null);
     ButterKnife.bind(this, view);
+    lineList.add(mLineView1);
+    lineList.add(mLineView2);
+    lineList.add(mLineView3);
+    lineList.add(mLineView4);
+
     return view;
   }
 
   @Override public void onDestroyView() {
     ButterKnife.unbind(this);
     super.onDestroyView();
+  }
+
+  public void toggleLine(int position) {
+    for (View view : lineList) {
+      view.setVisibility(View.INVISIBLE);
+    }
+    lineList.get(position).setVisibility(View.VISIBLE);
+  }
+  public class NewsSummartAdapter extends CommonAdapter<NewsSummary>{
+
+    public NewsSummartAdapter(Context context, List<NewsSummary> list, int resID) {
+      super(context, list, resID);
+    }
+
+    @Override
+    public void initView(NewsSummary newsSummary, int position, CommonViewHolder viewHolder) {
+
+      }
   }
 }
--- app/src/main/java/com/zhicall/hax/net/INewsService.java
+++ app/src/main/java/com/zhicall/hax/net/INewsService.java
@@ -1,10 +1,13 @@
 package com.zhicall.hax.net;
 
+import android.support.annotation.Nullable;
 import com.zhicall.hax.bean.NewsCategory;
+import com.zhicall.hax.bean.NewsSummary;
 import com.zhicall.hax.bean.Result;
 import java.util.List;
 import retrofit.http.GET;
 import retrofit.http.Headers;
+import retrofit.http.Query;
 import rx.Observable;
 
 /**
@@ -12,7 +15,10 @@ import rx.Observable;
  * Email:huangjinxin@zhicall.cn
  */
 public interface INewsService {
-  @GET("/tngou/info/classify")
-  @Headers({"apikey:d99a9ad0a9531fcbd1bb3139651e7249"})
+  @GET("/tngou/info/classify") @Headers({ "apikey:d99a9ad0a9531fcbd1bb3139651e7249" })
   Observable<Result<List<NewsCategory>>> getNewsCategory();
+
+  @GET("/tngou/info/classify") @Headers({ "apikey:d99a9ad0a9531fcbd1bb3139651e7249" })
+  Observable<Result<List<NewsSummary>>> getNewsSummary(@Nullable @Query("id") int id,
+      @Nullable @Query("page") int page, @Nullable @Query("rows") int rows);
 }
