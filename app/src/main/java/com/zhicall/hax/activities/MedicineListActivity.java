d23fcb5fa509eb9474c07bb1d9645f69f23889ee
Draven1122 <hjx931122@vip.qq.com>
2016-01-14 13:04:34 +0800

    update

---
+++ app/src/main/java/com/zhicall/hax/activities/MedicineListActivity.java
@@ -0,0 +1,18 @@
+package com.zhicall.hax.activities;
+
+import android.os.Bundle;
+import com.zhicall.hax.BaseActivity;
+
+/**
+ * Created by Xingchen on 2016/1/14.
+ * Email:huangjinxin@zhicall.cn
+ */
+public class MedicineListActivity extends BaseActivity {
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override public void initView() {
+
+  }
+}
