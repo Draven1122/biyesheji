0378ee88aaa5c5861072a468b1540119998faab1
Draven1122 <hjx931122@vip.qq.com>
2016-01-19 15:07:57 +0800

    update

--- app/src/main/java/com/zhicall/hax/activities/DiseaseListActivity.java
+++ app/src/main/java/com/zhicall/hax/activities/DiseaseListActivity.java
@@ -1,5 +1,6 @@
 package com.zhicall.hax.activities;
 
+import android.os.Bundle;
 import com.zhicall.hax.BaseActivity;
 
 /**
@@ -7,6 +8,11 @@ import com.zhicall.hax.BaseActivity;
  * Email:huangjinxin@zhicall.cn
  */
 public class DiseaseListActivity extends BaseActivity {
+  private int mCurrentPage=1;
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
   @Override public void initData() {
 
   }
---
+++ app/src/main/java/com/zhicall/hax/bean/Disease.java
@@ -0,0 +1,152 @@
+package com.zhicall.hax.bean;
+
+/**
+ * Created by Xingchen on 2016/1/19.
+ * Email:huangjinxin@zhicall.cn
+ */
+public class Disease {
+  int count;
+  String department;
+  String description;
+  String disease;
+  String diseasetext;
+  String caretext;
+  String causetext;
+  String checks;
+  String checktext;
+  String food;
+  String foodtext;
+  String drug;
+  String drugtext;
+  String symptom;
+  String symptomtext;
+  int fcount;
+  int id;
+  String img;
+  String keywords;
+  String message;
+  String name;
+  String place;
+  int rcount;
+
+  public int getCount() {
+    return count;
+  }
+
+  public String getDepartment() {
+    return department;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getDisease() {
+    return disease;
+  }
+
+  public String getCaretext() {
+    return caretext;
+  }
+
+  public String getDiseasetext() {
+    return diseasetext;
+  }
+
+  public String getCausetext() {
+    return causetext;
+  }
+
+  public String getChecks() {
+    return checks;
+  }
+
+  public String getChecktext() {
+    return checktext;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public String getFoodtext() {
+    return foodtext;
+  }
+
+  public String getDrug() {
+    return drug;
+  }
+
+  public String getDrugtext() {
+    return drugtext;
+  }
+
+  public String getSymptom() {
+    return symptom;
+  }
+
+  public String getSymptomtext() {
+    return symptomtext;
+  }
+
+  public int getFcount() {
+    return fcount;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getImg() {
+    return img;
+  }
+
+  public String getKeywords() {
+    return keywords;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPlace() {
+    return place;
+  }
+
+  public int getRcount() {
+    return rcount;
+  }
+}
+/*
+
+
+{ "total":5801,
+"tngou":[
+{"count":0,
+"department":"妇科",
+"description":"",
+"disease":"",
+"diseasetext":"",
+"caretext":"",
+"causetext":"",
+"checks":"",
+"checktext":"",
+"food":"",
+"foodtext":"",
+"drug":"",
+"drugtext":"",
+"symptom":"",
+"symptomtext":"",
+"fcount":0,
+"id":5801,
+"img":"/operation/080819/d9101510fc088e67ab3adea7b68f50bf.jpg",//【1】
+"keywords":"",
+"message":"",
+"name":"",
+"place":"子宫",
+"rcount":0}]}
+ */
